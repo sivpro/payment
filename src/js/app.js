bba530bdd95be6a55d5d3ef23dec15d78852b34c
acrysense <54480354+acrysense@users.noreply.github.com>
2021-09-30 14:22:02 +0300

    init: Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,132 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // INPUTMASK
+    Inputmask().mask(document.querySelectorAll('input'));
+
+    // HEIGHT 100VH FIX FOR IOS
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    window.addEventListener('resize', () => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+    
+    // SMOOTH SCROLL
+    function currentYPosition() {
+        // Firefox, Chrome, Opera, Safari
+        if (self.pageYOffset) return self.pageYOffset;
+
+        // Internet Explorer 6 - standards mode
+        if (document.documentElement && document.documentElement.scrollTop)
+            return document.documentElement.scrollTop;
+        
+        // Internet Explorer 6, 7 and 8
+        if (document.body.scrollTop) return document.body.scrollTop;
+
+        return 0;
+    } 
+    
+    function elmYPosition(eID) {
+        let elm = document.getElementById(eID);
+        let y = elm.offsetTop;
+        let node = elm;
+        while (node.offsetParent && node.offsetParent != document.body) {
+            node = node.offsetParent;
+            y += node.offsetTop;
+        } return y;
+    }
+    
+    function smoothScroll(eID) {
+        let startY = currentYPosition();
+        let stopY = elmYPosition(eID) - Number(document.querySelector('.header').clientHeight);
+        let distance = stopY > startY ? stopY - startY : startY - stopY;
+        if (distance < 100) {
+            scrollTo(0, stopY); return;
+        }
+        let speed = Math.round(distance / 100);
+        if (speed >= 20) speed = 20;
+        let step = Math.round(distance / 25);
+        let leapY = stopY > startY ? startY + step : startY - step;
+        let timer = 0;
+        if (stopY > startY) {
+            for (let i = startY; i < stopY; i += step ) {
+                setTimeout('window.scrollTo(0, ' + leapY + ')', timer * speed);
+                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+            } return;
+        }
+        for (let i = startY; i > stopY; i -= step ) {
+            setTimeout('window.scrollTo(0, ' + leapY + ')', timer * speed);
+            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        }
+    }
+    
+    function smoothScrollCoord(coord) {
+        let startY = currentYPosition();
+        let stopY = coord;
+        let distance = stopY > startY ? stopY - startY : startY - stopY;
+        if (distance < 100) {
+            scrollTo(0, stopY); return;
+        }
+        let speed = Math.round(distance / 100);
+        if (speed >= 20) speed = 20;
+        let step = Math.round(distance / 25);
+        let leapY = stopY > startY ? startY + step : startY - step;
+        let timer = 0;
+        if (stopY > startY) {
+            for (let i = startY; i < stopY; i += step ) {
+                setTimeout('window.scrollTo(0, ' + leapY + ')', timer * speed);
+                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+            } return;
+        }
+        for (let i = startY; i > stopY; i -= step ) {
+            setTimeout('window.scrollTo(0, ' + leapY + ')', timer * speed);
+            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        }
+    }
+
+    // ALL LINKS SMOOTH SCROLL
+    const allLinks = document.querySelectorAll('a[href^="#"]')
+
+    if (allLinks) {
+        allLinks.forEach(item => {
+            item.addEventListener('click', (event) => {
+                event.preventDefault()
+        
+                if (item.getAttribute('href').length > 1) {
+                    smoothScroll(item.getAttribute('href').slice(1))
+                }
+            })
+        })
+    }
+
+    // HEADER HEIGHT
+    const header = document.querySelector('.header')
+
+    document.documentElement.style.setProperty('--height', `${header.getBoundingClientRect().height}px`);
+    
+    // SWIPER
+    const reportSlider = document.querySelector('.report__slider .swiper-container')
+
+    if (reportSlider) {
+        const mySwiperReport = new Swiper(reportSlider, {
+            spaceBetween: 50,
+            slidesPerView: 2,
+            centeredSlides: true,
+            roundLengths: true,
+            loop: true,
+            loopAdditionalSlides: 30,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            breakpoints: {
+                0: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 2,
+                }
+            },
+        })
+    }
+});
